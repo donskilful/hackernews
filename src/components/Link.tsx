babbf44591f66a97ec9e4fd09383ce28267b632f
donskilful <donskilful@gmail.com>
2022-07-24 20:14:10 +0100

    final commit maybe lol

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,15 +1,68 @@
-type LinkProps = {
-    link: { description: string, url: string },
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
+
+type vote = [
+  id:string,
+  user:{
+    id:string
+  }
+]
+
+type postedBy = {
+  id:string
+  name:string
 }
+
+type LinkProps = {
+  link: {
+    index:number
+    description: string;
+    url: string;
+    createdAt:string
+    votes:vote
+    postedBy:postedBy
+  };
+};
 
 const Link = ({ link }: LinkProps) => {
+  console.log("Link: ", link);
+
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    // <div>
+    //   <div>
+    //     {link.description} ({link.url})
+    //   </div>
+    // </div>
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{link.index + 1}.</span>
+        {authToken && (
+          <div
+            className="ml1 gray f11"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              console.log("Clicked vote button");
+            }}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        {
+          <div className="f6 lh-copy gray">
+            {link.votes.length} votes | by{" "}
+            {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+            {timeDifferenceForDate(link.createdAt)}
+          </div>
+        }
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Link
+export default Link;
--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,7 +1,7 @@
 import Link from "./Link";
 import { useQuery, gql, QueryResult } from "@apollo/client";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       id
@@ -10,6 +10,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -21,37 +31,53 @@ const FEED_QUERY = gql`
 //   image?: string;
 // };
 
-type Links = {
-    createdAt:string
-    description:string
+
+type votes =[
+  id:string,
+  user: {
     id:string
-    url:string
+  }
+]
+
+type postedBy = {
+  id:string
+  name:string
 }
 
+type Links = {
+  index:number
+  createdAt: string;
+  description: string;
+  id: string;
+  url: string;
+  postedBy:postedBy
+  votes:votes
+};
+
 type data = {
-        feed: {
-          id: string;
-          links: Links[]
-        };
+  feed: {
+    id: string;
+    links: Links[];
+  };
 };
 
 // type LinksToRender = Array<LinkItem>;
 
 const LinkList = () => {
-  const { data } : QueryResult<data> = useQuery(FEED_QUERY);
+  const { data }: QueryResult<data> = useQuery(FEED_QUERY);
 
-//   const linksToRender: LinksToRender = [
-//     {
-//       id: "link-id-1",
-//       description: "Prisma gives you a powerful database toolkit 😎",
-//       url: "https://prisma.io",
-//     },
-//     {
-//       id: "link-id-2",
-//       description: "The best GraphQL client",
-//       url: "https://www.apollographql.com/docs/react/",
-//     },
-//   ];
+  //   const linksToRender: LinksToRender = [
+  //     {
+  //       id: "link-id-1",
+  //       description: "Prisma gives you a powerful database toolkit 😎",
+  //       url: "https://prisma.io",
+  //     },
+  //     {
+  //       id: "link-id-2",
+  //       description: "The best GraphQL client",
+  //       url: "https://www.apollographql.com/docs/react/",
+  //     },
+  //   ];
   return (
     <div>
       {data && (
